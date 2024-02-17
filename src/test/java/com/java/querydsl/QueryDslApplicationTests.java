ba164790cdb131ca94caa5df3b1cd61d99bc20a2
sososo0 <sh0000@pusan.ac.kr>
2024-02-17 20:19:43 +0900

    test: Query DSL 설치 test

--- src/test/java/com/java/querydsl/QueryDslApplicationTests.java
+++ src/test/java/com/java/querydsl/QueryDslApplicationTests.java
@@ -1,13 +1,35 @@
 package com.java.querydsl;
 
+import com.java.querydsl.entity.Hello;
+import com.java.querydsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 class QueryDslApplicationTests {
+
+    @Autowired
+    EntityManager em;
 
     @Test
     void contextLoads() {
-    }
+        Hello hello = new Hello();
+        em.persist(hello);
 
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        Assertions.assertThat(result).isEqualTo(hello);
+        Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
+    }
 }
